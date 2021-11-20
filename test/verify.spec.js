b762ee1b59b7901ffae057653a1f5b13f8640bd4
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2021-11-20 12:16:20 +0100

    test: update verify.spec.js

--- test/verify.spec.js
+++ test/verify.spec.js
@@ -12,10 +12,64 @@ test.onFinish(snapshot.restore);
 
 test("verify 'express' package", async(tape) => {
   const data = await verify("express@4.17.0");
-  delete data.directorySize;
+
+  tape.deepEqual(data.files, {
+    list: [
+      "History.md",
+      "LICENSE",
+      "Readme.md",
+      "index.js",
+      "lib\\application.js",
+      "lib\\express.js",
+      "lib\\middleware\\init.js",
+      "lib\\middleware\\query.js",
+      "lib\\request.js",
+      "lib\\response.js",
+      "lib\\router\\index.js",
+      "lib\\router\\layer.js",
+      "lib\\router\\route.js",
+      "lib\\utils.js",
+      "lib\\view.js",
+      "package.json"
+    ],
+    extensions: [".md", ".js", ".json"],
+    minified: []
+  });
+  tape.true(data.directorySize > 0);
+
+  // licenses
+  tape.deepEqual(data.uniqueLicenseIds, ["MIT"]);
+  tape.deepEqual(data.licenses, [
+    {
+      uniqueLicenseIds: ["MIT"],
+      spdxLicenseLinks: ["https://spdx.org/licenses/MIT.html#licenseText"],
+      spdx: {
+        osi: true,
+        fsf: true,
+        fsfAndOsi: true,
+        includesDeprecated: false
+      },
+      from: "package.json"
+    },
+    {
+      uniqueLicenseIds: ["MIT"],
+      spdxLicenseLinks: ["https://spdx.org/licenses/MIT.html#licenseText"],
+      spdx: {
+        osi: true,
+        fsf: true,
+        fsfAndOsi: true,
+        includesDeprecated: false
+      },
+      from: "LICENSE"
+    }
+  ]);
+
+  tape.true(data.ast.warnings.length === 2);
+  const warningName = data.ast.warnings.map((row) => row.kind);
+  tape.deepEqual(warningName, ["parsing-error", "unsafe-import"]);
 
   snapshot.core({
-    what: data,
+    what: JSON.stringify(data.ast.dependencies, null, 2),
     file: fileURLToPath(import.meta.url),
     specName: "verify express@4.17.0"
   });
