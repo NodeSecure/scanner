956551107185b1e62030342d5b12fb8bad141c5f
tekeuange23 <tekeuangeleonel@gmail.com>
2022-10-26 13:21:41 +0100

    refacto: starting deep comparison within the compare api

--- index.js
+++ index.js
@@ -74,20 +74,27 @@ export async function verify(packageName = null) {
 
 export function compare(payload1, payload2) {
   const [obj1, obj2] = [JSON.parse(payload1), JSON.parse(payload2)];
-
-  const obj1Keys = Object.keys(obj1).sort();
-  const obj2Keys = Object.keys(obj2).sort();
-
+  const [obj1Keys, obj2Keys] = [Object.keys(obj1).sort(), Object.keys(obj2).sort()];
   const objKeys = obj1Keys.concat(obj2Keys);
-  const changes = [];
 
+  const comparison = new Map();
+
+  // changes
+  const changes = [];
   for (const key of objKeys) {
     if (hasSomethingChanged(obj1, obj2, key) && changes.indexOf(key) === -1) {
       changes.push(key);
     }
   }
 
-  return changes;
+  // same package ?
+  const [obj1name, obj2name, samePackage] = obj1.name === obj2.name
+    ? [obj1.version, obj2.version, true]
+    : [obj1.name, obj2.name, false];
+
+  comparison.set("changes", changes);
+
+  return comparison;
 }
 
 export { depWalker, tarball, Logger, ScannerLoggerEvents };
--- src/utils/hasSomethingChanged.js
+++ src/utils/hasSomethingChanged.js
@@ -1,10 +1,42 @@
 // Import Node.js Dependencies
 import util from "util";
 
+export function compareDepVersions(str1, str2) {
+  const [digitsOnly1, digitsOnly2] = [str1.slice(1), str1.slice(1)];
+  const [digitsArr1, digitsArr2] = [digitsOnly1.split("."), digitsOnly2.split(".")];
+  for (let i = 0; i < 3; i++) {
+    const [digits1, digits2] = [parseInt(digitsArr1[i], 10), parseInt(digitsArr2[i], 10)];
+    if (digit1 > digit2) {
+      return {
+        from: str1,
+        to: str2,
+        status: "up to date"
+      };
+    }
+    else if (digit1 < digit2) {
+      return {
+        from: str1,
+        to: str2,
+        status: "outdated"
+      };
+    }
+  }
+}
+
 export function hasSomethingChanged(obj1, obj2, key) {
   if (util.isDeepStrictEqual(obj1[key], obj2[key])) {
     return false;
   }
 
   return true;
+}
+
+export function upgradedDepsFromPackage1ToPackage2(obj1Deps, obj2Deps) {
+  const upgrade = new Map();
+
+  for (const key in obj1Deps) {
+    if (Object.hasOwnProperty.call(obj2Deps, key)) {
+      const element = obj2Deps[key];
+    }
+  }
 }
