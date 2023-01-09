ac265603a0a5c5651e652fef708c9d7e16955495
Gentilhomme <gentilhomme.thomas@gmail.com>
2023-01-09 15:32:32 +0100

    fix(index.d.ts): add missing .js extension for NodeNext target (#90)

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-import Scanner from "./types/scanner";
-import { cwd, from, verify, ScannerLoggerEvents } from "./types/api";
-import { depWalker } from "./types/walker";
-import { Logger, LoggerEventData } from "./types/logger";
-import tarball from "./types/tarball";
+import Scanner from "./types/scanner.js";
+import { cwd, from, verify, ScannerLoggerEvents } from "./types/api.js";
+import { depWalker } from "./types/walker.js";
+import { Logger, LoggerEventData } from "./types/logger.js";
+import tarball from "./types/tarball.js";
 
 export {
   cwd, from, verify, ScannerLoggerEvents,
