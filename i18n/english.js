b86c9e74e16cbae4790727bd347438abff8a864d
fraxken <gentilhomme.thomas@gmail.com>
2023-06-23 21:16:59 +0200

    refactor: implement local i18n translations

---
+++ i18n/english.js
@@ -0,0 +1,6 @@
+const scanner = {
+  disable_scarf: "This dependency could collect data against your consent so think to disable it with the env var: SCARF_ANALYTICS",
+  keylogging: "This dependency can retrieve your keyboard and mouse inputs. It can be used for 'keylogging' attacks/malwares."
+};
+
+export default { scanner };
--- test/utils/warnings.spec.js
+++ test/utils/warnings.spec.js
@@ -2,6 +2,9 @@
 import { test } from "node:test";
 import assert from "node:assert";
 
+// Import Third-party Dependencies
+import * as i18n from "@nodesecure/i18n";
+
 // Require Internal Dependencies
 import { getDependenciesWarnings } from "../../src/utils/index.js";
 
@@ -26,9 +29,8 @@ test("getDependenciesWarnings for '@scarf/scarf'", async() => {
   const warnsArray = await getDependenciesWarnings(deps);
   assert.strictEqual(warnsArray.warnings.length, 1);
 
-  assert.strictEqual(
-    warnsArray.warnings[0],
-    // eslint-disable-next-line max-len
-    "The dependency '@scarf/scarf' has been detected in the dependency Tree. This dependency could collect data against your will so think to disable it with the env var: SCARF_ANALYTICS"
+  const message = await i18n.getToken("scanner.disable_scarf");
+  assert.ok(
+    warnsArray.warnings[0].includes(message)
   );
 });
