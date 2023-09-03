df337646d6e52f9f298dd4bfdd6035ef416e01db
fraxken <gentilhomme.thomas@gmail.com>
2023-09-03 16:53:56 +0200

    feat: implement RC for flaggedAuthors

--- test/utils/warnings.spec.js
+++ test/utils/warnings.spec.js
@@ -4,6 +4,7 @@ import assert from "node:assert";
 
 // Import Third-party Dependencies
 import * as i18n from "@nodesecure/i18n";
+import * as rc from "@nodesecure/rc";
 
 // Require Internal Dependencies
 import { getDependenciesWarnings } from "../../src/utils/index.js";
@@ -21,16 +22,43 @@ function createDependency(maintainers = [], publishers = []) {
   };
 }
 
-test("getDependenciesWarnings for '@scarf/scarf'", async() => {
+// test("getDependenciesWarnings for '@scarf/scarf'", async() => {
+//   const deps = new Map([
+//     ["@scarf/scarf", createDependency()]
+//   ]);
+
+//   const { warnings, flaggedAuthors } = await getDependenciesWarnings(deps);
+//   assert.strictEqual(flaggedAuthors.length, 0);
+//   assert.strictEqual(warnings.length, 1);
+
+//   const message = await i18n.getToken("scanner.disable_scarf");
+//   assert.ok(
+//     warnings.at(0).includes(message)
+//   );
+// });
+
+test("getDependenciesWarnings (flag author)", async() => {
   const deps = new Map([
-    ["@scarf/scarf", createDependency()]
+    ["foobar", createDependency()]
   ]);
 
-  const warnsArray = await getDependenciesWarnings(deps);
-  assert.strictEqual(warnsArray.warnings.length, 1);
+  rc.memoize({
+    scanner: {
+      flaggedAuthors: [
+        {
+          name: "John Doe",
+          email: "john.doe@gmail.com"
+        }
+      ]
+    }
+  });
 
-  const message = await i18n.getToken("scanner.disable_scarf");
-  assert.ok(
-    warnsArray.warnings[0].includes(message)
-  );
+  const { warnings, flaggedAuthors } = await getDependenciesWarnings(deps);
+  console.log(flaggedAuthors);
+
+  assert.strictEqual(warnings.length, 0);
+  assert.strictEqual(flaggedAuthors.length, 1);
+
+  console.log(flaggedAuthors);
 });
+
