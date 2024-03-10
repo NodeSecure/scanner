a47439196098057f46af5250a7f215de327937fa
Jean <110341611+jean-michelet@users.noreply.github.com>
2024-03-10 23:20:12 +0100

    feat(scanner): make a deep comparison of two payloads (#211)
    * feat: draft of a feature to compare payloads
    
    * feat: add devDependency and author comparison
    
    * feat: add engines version comparison
    
    * test: should throw an error if compared payloads have the same id
    
    * feat: should detect warnings diff
    
    * feat: should detect warning diff in versions dependencies
    
    * fix: root warning are strings
    
    * test: should detect vulnerabilities diff
    
    * test: should detect vulnerabilities diff
    
    * fix: add default values to comparaison fonctions in case of non-existing prop in payload
    
    * feat: should detect flagged authors diff
    
    * feat: should detect scanner version diff
    
    * feat: should detect uniqueLicenseIds diff
    
    * feat: should detect vulnerability strategy version diff
    
    * feat: should detect existOnRemoteRegistry diff
    
    * feat: should detect description version diff
    
    * feat: should detect repository version diff
    
    * feat: should detect scripts version diff
    
    * feat: should detect flags version diff
    
    * feat: should detect dependency links version diff
    
    * feat: should detect version id diff
    
    * feat: should detect version size diff
    
    * feat: should detect version composition diff
    
    * refactor: improve comparison and diff + function naming
    
    * feat: create types
    
    * feat: create types
    
    * fix: remove unsued Vulnerability interface in scanner types
    
    * fix: added and removed diff are never undefined
    
    * fix: DictionaryComparison instead of DictionaryComparisonResult
    
    * fix: rename FIXTURE_PATH to kFixturePath
    
    Co-authored-by: PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
    
    * fix: rename payload to kPayload
    
    Co-authored-by: PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
    
    * refactor: split test cases
    
    ---------
    
    Co-authored-by: PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>

--- types/api.d.ts
+++ types/api.d.ts
@@ -1,15 +1,16 @@
-import Scanner from "./scanner.js";
+import Scanner, { Payload, PayloadComparison } from "./scanner.js";
 import { Logger, LoggerEvents } from "./logger.js";
 
 export {
   cwd,
   from,
   verify,
-  ScannerLoggerEvents
+  comparePayloads,
+  ScannerLoggerEvents,
 }
-
 declare const ScannerLoggerEvents: LoggerEvents;
 
 declare function cwd(location: string, options?: Scanner.Options, logger?: Logger): Promise<Scanner.Payload>;
 declare function from(packageName: string, options?: Omit<Scanner.Options, "includeDevDeps">, logger?: Logger): Promise<Scanner.Payload>;
 declare function verify(packageName?: string | null): Promise<Scanner.VerifyPayload>;
+declare function comparePayloads(original: Payload, toCompare: Payload): PayloadComparison
