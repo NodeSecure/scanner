c6bdaf95cc1d94006004c9e3f103d6afc477cb75
Thomas.G <gentilhomme.thomas@gmail.com>
2024-12-24 16:37:44 +0100

    fix(rc): export missing configs types (#330)

--- workspaces/rc/src/index.ts
+++ workspaces/rc/src/index.ts
@@ -3,4 +3,13 @@ export * from "./functions/write.js";
 export * from "./functions/memoize.js";
 export * as CONSTANTS from "./constants.js";
 
-export { type RC, JSONSchema, homedir } from "./rc.js";
+export {
+  type RC,
+  type CiConfiguration,
+  type CiWarnings,
+  type ReportConfiguration,
+  type ReportChart,
+  type ScannerConfiguration,
+  JSONSchema,
+  homedir
+} from "./rc.js";
