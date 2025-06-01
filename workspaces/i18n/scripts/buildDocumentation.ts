a9c35c089226b277dca29f93493695f3cb168d7a
Pierre Demailly <pierredemailly.pro@gmail.com>
2025-06-01 15:40:34 +0200

    chore(i18n): add cli & scanner tokens to the documentation

--- workspaces/i18n/scripts/buildDocumentation.ts
+++ workspaces/i18n/scripts/buildDocumentation.ts
@@ -1,6 +1,8 @@
 // Import Node.js Dependencies
 import path from "node:path";
 import fs from "node:fs";
+import os from "node:os";
+import url from "node:url";
 import { fileURLToPath } from "node:url";
 
 // Import Third-party Dependencies
@@ -17,6 +19,11 @@ const kTokens = {
   english,
   french
 };
+const kExternalI18nRepos = {
+  scanner: "workspaces/scanner/src/i18n",
+  cli: "i18n"
+};
+const kTaggedStringPath = url.pathToFileURL(path.join(import.meta.dirname, "../src/utils.js"));
 
 function flatten(
   obj: any,
@@ -44,6 +51,24 @@ function formatValue(
   const placeholders = Array.from({ length: 9 }, (_value, index) => `{${index}}`);
 
   return typeof obj[key] === "function" ? obj[key](...placeholders) : obj[key];
+}
+
+for (const [repo, i18nPath] of Object.entries(kExternalI18nRepos)) {
+  const frReq = await fetch(`https://raw.githubusercontent.com/NodeSecure/${repo}/refs/heads/master/${i18nPath}/french.js`);
+  const frRaw = await frReq.text();
+  const enReq = await fetch(`https://raw.githubusercontent.com/NodeSecure/${repo}/refs/heads/master/${i18nPath}/english.js`);
+  const enRaw = await enReq.text();
+
+  const tmpPathFr = path.join(os.tmpdir(), `fr-${repo}`);
+  const tmpPathEn = path.join(os.tmpdir(), `en-${repo}`);
+  fs.writeFileSync(tmpPathFr, frRaw.replace(`from "@nodesecure/i18n";`, `from "${kTaggedStringPath}"`));
+  fs.writeFileSync(tmpPathEn, enRaw.replace(`from "@nodesecure/i18n";`, `from "${kTaggedStringPath}"`));
+
+  const { default: fr } = await import(url.pathToFileURL(tmpPathFr).href);
+  const { default: en } = await import(url.pathToFileURL(tmpPathEn).href);
+
+  Object.assign(kTokens.french, { [repo]: fr[repo] });
+  Object.assign(kTokens.english, { [repo]: en[repo] });
 }
 
 const HTMLStr = fs.readFileSync(path.join(kProjectRootDir, "views", "index.html"), "utf-8");
