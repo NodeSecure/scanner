c65128ee15700aebf30310da7d12265ca5bf6a9c
fraxken <gentilhomme.thomas@gmail.com>
2025-06-13 19:13:22 +0200

    refactor(conformance/extract): support LocatedManifestManager along with string location

--- workspaces/conformance/src/extract.ts
+++ workspaces/conformance/src/extract.ts
@@ -4,7 +4,10 @@ import * as fsSync from "node:fs";
 import * as fs from "node:fs/promises";
 
 // Import Third-party Dependencies
-import { ManifestManager } from "@nodesecure/mama";
+import {
+  ManifestManager,
+  type LocatedManifestManager
+} from "@nodesecure/mama";
 
 // Import Internal Dependencies
 import * as utils from "./utils/index.js";
@@ -24,14 +27,15 @@ export interface ExtractAsyncOptions {
 }
 
 export async function extractLicenses(
-  location: string,
+  manifestOrLocation: string | LocatedManifestManager,
   options: ExtractAsyncOptions = {}
 ): Promise<SpdxExtractedResult> {
   const { fsEngine = fs } = options;
 
   const mama = await ManifestManager.fromPackageJSON(
-    location
+    manifestOrLocation
   );
+  const location = mama.location!;
 
   const licenseData = new LicenseResult()
     .addLicenseID(
@@ -60,16 +64,15 @@ export interface ExtractSyncOptions {
 }
 
 export function extractLicensesSync(
-  location: string,
+  manifestOrLocation: string | LocatedManifestManager,
   options: ExtractSyncOptions = {}
 ): SpdxExtractedResult {
   const { fsEngine = fsSync } = options;
 
-  const packageStr = fsEngine.readFileSync(
-    path.join(location, kManifestFileName), "utf-8"
+  const mama = ManifestManager.fromPackageJSONSync(
+    manifestOrLocation
   );
-  const packageJSON = JSON.parse(packageStr);
-  const mama = new ManifestManager(packageJSON);
+  const location = mama.location!;
 
   const licenseData = new LicenseResult();
   licenseData.addLicenseID(
--- workspaces/conformance/test/extract.spec.ts
+++ workspaces/conformance/test/extract.spec.ts
@@ -4,6 +4,9 @@ import assert from "node:assert";
 import { fileURLToPath } from "node:url";
 import { describe, it } from "node:test";
 
+// Import Third-party Dependencies
+import { ManifestManager } from "@nodesecure/mama";
+
 // Import Internal Dependencies
 import {
   extractLicenses,
@@ -14,33 +17,73 @@ import expectedParsedLicense from "./fixtures/parseLicense.snap.js";
 // CONSTANTS
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const kFixturePath = path.join(__dirname, "fixtures");
+const kFixtureProjectOne = path.join(kFixturePath, "project1");
+const kFixtureProjectTwo = path.join(kFixturePath, "project2");
 
 describe("extractLicenses", () => {
   it("should detect two licenses (ISC, MIT) in project1", async() => {
-    const result = await extractLicenses(path.join(kFixturePath, "project1"));
+    const result = await extractLicenses(kFixtureProjectOne);
+
+    assert.deepStrictEqual(result.uniqueLicenseIds, ["ISC", "MIT"]);
+    assert.deepStrictEqual(result, expectedParsedLicense);
+  });
+
+  it("should detect two licenses (ISC, MIT) in project1 using LocatedManifestManager", async() => {
+    const mama = await ManifestManager.fromPackageJSON(
+      kFixtureProjectOne
+    );
+    if (!ManifestManager.isLocated(mama)) {
+      throw new Error("manifest must have a location");
+    }
+
+    const result = await extractLicenses(mama);
 
     assert.deepStrictEqual(result.uniqueLicenseIds, ["ISC", "MIT"]);
     assert.deepStrictEqual(result, expectedParsedLicense);
   });
 
   it("should detect one license (Artistic-2.0) in project2", async() => {
-    const result = await extractLicenses(path.join(kFixturePath, "project2"));
+    const result = await extractLicenses(
+      kFixtureProjectTwo
+    );
 
-    assert.deepStrictEqual(result.uniqueLicenseIds, ["Artistic-2.0"]);
+    assert.deepStrictEqual(
+      result.uniqueLicenseIds,
+      ["Artistic-2.0"]
+    );
   });
 });
 
 describe("extractLicensesSync", () => {
-  it("should detect two licenses (ISC, MIT) in project1", async() => {
-    const result = extractLicensesSync(path.join(kFixturePath, "project1"));
+  it("should detect two licenses (ISC, MIT) in project1", () => {
+    const result = extractLicensesSync(kFixtureProjectOne);
 
     assert.deepStrictEqual(result.uniqueLicenseIds, ["ISC", "MIT"]);
     assert.deepStrictEqual(result, expectedParsedLicense);
   });
 
-  it("should detect one license (Artistic-2.0) in project2", async() => {
-    const result = extractLicensesSync(path.join(kFixturePath, "project2"));
+  it("should detect two licenses (ISC, MIT) in project1 using LocatedManifestManager", () => {
+    const mama = ManifestManager.fromPackageJSONSync(
+      kFixtureProjectOne
+    );
+    if (!ManifestManager.isLocated(mama)) {
+      throw new Error("manifest must have a location");
+    }
 
-    assert.deepStrictEqual(result.uniqueLicenseIds, ["Artistic-2.0"]);
+    const result = extractLicensesSync(mama);
+
+    assert.deepStrictEqual(result.uniqueLicenseIds, ["ISC", "MIT"]);
+    assert.deepStrictEqual(result, expectedParsedLicense);
+  });
+
+  it("should detect one license (Artistic-2.0) in project2", () => {
+    const result = extractLicensesSync(
+      kFixtureProjectTwo
+    );
+
+    assert.deepStrictEqual(
+      result.uniqueLicenseIds,
+      ["Artistic-2.0"]
+    );
   });
 });
