72172897bb1b75d98d2c8797e077d20f7e15ab4d
Clement Gombauld <clementgombauld@hotmail.fr>
2025-07-23 13:44:22 +0200

    feat(contact): add fromManifest method in extractor (#494)
    * feat(contact): add fromManifest method in extractor
    
    * Update workspaces/contact/test/ContactExtractor.spec.ts
    
    Co-authored-by: PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: PierreDemailly <39910767+PierreDemailly@users.noreply.github.com>

--- workspaces/contact/src/ContactExtractor.class.ts
+++ workspaces/contact/src/ContactExtractor.class.ts
@@ -1,5 +1,5 @@
 // Import Third-party Dependencies
-import type { Contact } from "@nodesecure/npm-types";
+import type { Contact, PackumentVersion } from "@nodesecure/npm-types";
 
 // Import Internal Dependencies
 import {
@@ -18,6 +18,8 @@ export interface ContactExtractorPackageMetadata {
   author?: Contact;
   maintainers: Contact[];
 }
+
+type ContactPackageMetaData = Partial<ContactExtractorPackageMetadata>;
 
 export interface ContactExtractorFromDependenciesResult {
   illuminated: IlluminatedContact[];
@@ -54,16 +56,39 @@ export class ContactExtractor {
     for (const [packageName, metadata] of Object.entries(dependencies)) {
       const extractedContacts = extractMetadataContacts(metadata);
       extractedContacts.forEach((contact) => resolver.registerEmail(contact.email));
-
-      for (const unlit of unlitContacts) {
-        const isMaintainer = extractedContacts
-          .some((contact) => unlit.compareTo(contact));
-        if (isMaintainer) {
-          unlit.dependencies.add(packageName);
-        }
-      }
+      this.addDependencyToUnlitContacts(unlitContacts, extractedContacts, packageName);
     }
 
+    return this.processIlluminatedAndExpired(unlitContacts, resolver);
+  }
+
+  async fromManifest(manifest: PackumentVersion) {
+    const resolver = new NsResolver();
+    const unlitContacts = this.highlighted
+      .map((contact) => new UnlitContact(contact));
+
+    const extractedContacts = extractMetadataContacts(manifest);
+
+    extractedContacts.forEach((contact) => resolver.registerEmail(contact.email));
+    this.addDependencyToUnlitContacts(unlitContacts, extractedContacts, manifest.name);
+
+    return this.processIlluminatedAndExpired(unlitContacts, resolver);
+  }
+
+  private addDependencyToUnlitContacts(
+    unlitContacts: UnlitContact[],
+    contacts: Contact[],
+    packageName: string
+  ) {
+    for (const unlit of unlitContacts) {
+      const isMaintainer = contacts.some((contact) => unlit.compareTo(contact));
+      if (isMaintainer) {
+        unlit.dependencies.add(packageName);
+      }
+    }
+  }
+
+  private async processIlluminatedAndExpired(unlitContacts: UnlitContact[], resolver: NsResolver) {
     const expired = await resolver.getExpired();
 
     const illuminated = unlitContacts.flatMap(
@@ -78,10 +103,10 @@ export class ContactExtractor {
 }
 
 function extractMetadataContacts(
-  metadata: ContactExtractorPackageMetadata
+  metadata: ContactPackageMetaData
 ): Contact[] {
   return [
     ...(metadata.author ? [metadata.author] : []),
-    ...metadata.maintainers
+    ...(metadata.maintainers ? metadata.maintainers : [])
   ];
 }
