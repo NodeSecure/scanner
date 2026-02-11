a69f6dd481822d1d6fe788fdde27db9bc77f4bec
cgombauld <clement.gombauld@cpexterne.org>
2026-02-11 16:56:54 +0100

    feat(scanner): emit an error event when an IO fails

--- workspaces/scanner/src/class/StatsCollector.class.ts
+++ workspaces/scanner/src/class/StatsCollector.class.ts
@@ -1,55 +1,82 @@
+// Import Node.js Dependencies
+import { EventEmitter } from "node:events";
+
 // Import Third-party Dependencies
 import { isHTTPError } from "@openally/httpie";
 
 // Import Internal Dependencies
 import { SystemDateProvider, type DateProvider } from "./DateProvider.class.ts";
+import type { LoggerEventsMap } from "./logger.class.ts";
 import type { ApiStats, Stats, Error } from "../types.ts";
 
 export class StatsCollector {
-  #apiCalls: ApiStats[] = [];
+  #logger: EventEmitter<LoggerEventsMap>;
   #dateProvider: DateProvider;
+  #apiCalls: ApiStats[] = [];
   #startedAt: number;
   #errors: Error[] = [];
 
-  constructor(dateProvider: DateProvider = new SystemDateProvider()) {
+  constructor(logger: EventEmitter<LoggerEventsMap>, dateProvider: DateProvider = new SystemDateProvider()) {
+    this.#logger = logger;
     this.#dateProvider = dateProvider;
     this.#startedAt = this.#dateProvider.now();
   }
 
-  track<T extends () => any>(name: string, fn: T): ReturnType<T> {
+  track<T extends () => any>(name: string, phase: string, fn: T): ReturnType<T> {
     const startedAt = this.#dateProvider.now();
     try {
       const result = fn();
       if (result instanceof Promise) {
         return result
-          .catch((err) => {
-            this.#addError(name, err);
-            throw err;
+          .then((res: ReturnType<T>) => {
+            this.#addApiStat(name, startedAt, this.#calcExecutionTime(startedAt));
+
+            return res;
           })
-          .finally(() => this.#addApiStat(name, startedAt)
-          ) as ReturnType<T>;
+          .catch((err) => {
+            const executionTime = this.#calcExecutionTime(startedAt);
+            this.#addError({
+              name, err, executionTime, phase
+            });
+            this.#addApiStat(name, startedAt, executionTime);
+
+            throw err;
+          }) as ReturnType<T>;
       }
 
-      this.#addApiStat(name, startedAt);
+      this.#addApiStat(name, startedAt, this.#calcExecutionTime(startedAt));
 
       return result;
     }
     catch (err) {
-      this.#addApiStat(name, startedAt);
-      this.#addError(name, err);
+      const executionTime = this.#calcExecutionTime(startedAt);
+      this.#addApiStat(name, startedAt, executionTime);
+      this.#addError({
+        name, err, executionTime, phase
+      });
       throw err;
     }
   }
 
-  #addApiStat(name: string, startedAt: number) {
+  #calcExecutionTime(startedAt: number) {
+    return this.#dateProvider.now() - startedAt;
+  }
+
+  #addApiStat(name: string, startedAt: number, executionTime: number) {
     this.#apiCalls.push({
       name,
       startedAt,
-      executionTime: this.#dateProvider.now() - startedAt
+      executionTime
     });
   }
 
-  #addError(name: string, err: unknown) {
+  #addError(params: {
+    name: string;
+    executionTime: number;
+    err: unknown;
+    phase: string | undefined;
+  }) {
+    const { name, executionTime, err, phase } = params;
     const error: Error = {
       name
     };
@@ -61,6 +88,7 @@ export class StatsCollector {
       error.statusCode = err.statusCode;
     }
     this.#errors.push(error);
+    this.#logger.emit("error", { ...error, executionTime }, phase);
   }
 
   getStats(): Stats {
